209872dce71d18970f96b6b7860ac5cee0c722c4
tu21 <36232648+tu21@users.noreply.github.com>
2018-02-14 22:01:03 +0300

    تعديل الترك بيج

--- app/src/main/java/com/example/z7n/foodtruck/Fragments/TruckPageFragment.java
+++ app/src/main/java/com/example/z7n/foodtruck/Fragments/TruckPageFragment.java
@@ -6,17 +6,45 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.z7n.foodtruck.Activity.MainActivity;
+import com.example.z7n.foodtruck.LoginState;
+import com.example.z7n.foodtruck.R;
+import com.example.z7n.foodtruck.Truck;
 
 /**
  */
 
 public class TruckPageFragment extends Fragment {
 
+    private View parentView;
+    private Truck truck;
+    private  TextView truckName;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        parentView = inflater.inflate(R.layout.fragment_truck_profile, container, false);
+        truckName = parentView.findViewById(R.id.truckName);
+        initTruck();
+
+        truckName.setText(truck.getTruckName());
+
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    private void initTruck() {
+
+        if(getActivity() == null)
+            return;
+
+        LoginState loginState = ((MainActivity)getActivity()).getLoginState();
+
+        if( !loginState.isTruck() || loginState.isVisitor())
+            return;
+
+        truck = loginState.getTruck();
     }
 }
